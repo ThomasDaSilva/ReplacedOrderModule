2011bf3d7565a5a6edbe1595c2d2ce24147a72b8
Thomas Da Silva <tdasilva@openstudio.fr>
2024-06-26 14:46:22 +0200

    Fix insert sql + fix translation + fix php 8

--- I18n/backOffice/default/fr_FR.php
+++ I18n/backOffice/default/fr_FR.php
@@ -1,9 +1,9 @@
 <?php
 
 return array(
-    'Replace Module' => 'Module Remplacé',
-    'Replaced Order Module' => 'Remplacement de module',
+    'Replace Module' => 'Remplacer le Module',
+    'Replaced Order Module' => 'Remplacement de module de commandes',
     'Simple Replaced Order Module' => 'Remplacement de module simple',
-    'replaced the id of a the module by the id of ReplacedOrderModule' => 'remplace l\'id d\'un module par celui du module ReplacedOrderModule',
-    'replaced the id of a the module by the id of an other module' => 'remplace l\'id d\'un module par celui du module sélectionné',
+    'replaced the id of a the module by the id of ReplacedOrderModule' => 'Remplace l\'id d\'un module par l\'id du module ReplacedOrderModule',
+    'replaced the id of a the module by the id of an other module' => 'Remplace l\'id d\'un module par l\'id du module sélectionné',
 );
